61fb4a77e625c3e4b1e6a09c59ba921d0a24f87f
Enes <66560757+devenes@users.noreply.github.com>
2022-05-07 02:53:54 +0300

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,41 @@
+####################################
+#### Required AWS Configuration ####
+####################################
+
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "profile" {
+  default = "default"
+}
+
+variable "backend" {
+  default     = "terraform-env-bucket"
+  description = "The name of the S3 bucket to store Terraform state"
+}
+
+variable "tf_key" {
+  default     = "tf/terraform.tfstate"
+  description = "The key of the S3 bucket to store Terraform state"
+}
+
+########################################
+# Required AWS Resources Configuration #
+########################################
+
+variable "key_name" {
+  default = "virginia"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
+
+variable "instance_name" {
+  default = "Docker Web Server"
+}
+
+variable "security_group_name" {
+  default = "docker-sec-gr"
+}
